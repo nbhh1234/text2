f2a6f185c0ba49d04c457ce64cd8af599c9ae5ea
nbhh1234 <43559946+nbhh1234@users.noreply.github.com>
2018-10-12 14:31:10 +0800

    Update baolipojie.py

--- baolipojie.py
+++ baolipojie.py
@@ -1,3 +1,5 @@
+
+'''
 #_*_ coding:utf8-
 import crypt
 
@@ -23,4 +25,25 @@ def main():
 
 
 if __name__ == "__main__":
+    main()
+'''
+import crypt
+import itertools
+
+def main():
+    flag=0
+    salt='AB'
+    cryptPass='AB5I64J9ZNvp2'
+    #test=(''.join(x) for x in itertools.product("huwangW", repeat=7))
+    test=(''.join(x) for x in itertools.product("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM", repeat=7))
+    while flag==0:
+        word=next(test)
+        cryptWord = crypt.crypt(word,salt)
+        if cryptWord == cryptPass:
+            print('[+] Found Password:'+word)
+            flag=1
+        elif cryptWord == 'ABwpCvqmz77No':
+            print('[-] Not Found')
+
+if __name__ == '__main__':
     main()
